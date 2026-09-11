efbede7bca3c6c5891461431f248f511012dd2ea
aloksport <alokranjan999@gmail.com>
2026-09-11 21:35:44 +0530

    Initial PHP application

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+#FROM php:8.3-apache
+
+#COPY . /var/www/html/
+
+#EXPOSE 80
+
+FROM php:8.3-apache
+
+RUN a2enmod rewrite
+
+WORKDIR /var/www/html
+
+COPY . /var/www/html/
+
+RUN chown -R www-data:www-data /var/www/html
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//phpinfo();
+echo "<p>My php version is: " . phpversion() . "</p>";
+echo "<p>This is test from docker and </p>";
+echo "<p>Welcome to my website</p>";
+echo "<p>This is a simple PHP script running in a Docker container.</p>";
+echo "<p>You can customize this page to display your own content.</p>";
+echo "<p>Feel free to modify the code and add your own features.</p>";
+echo "<p>Thank you for visiting!</p>";
+?>
