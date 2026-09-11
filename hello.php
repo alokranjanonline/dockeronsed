9c2f71bb98c2689db3c57dc0e35e89af56206315
aloksport <alokranjan999@gmail.com>
2026-09-11 22:29:33 +0530

    Build and push Docker image

--- hello.php
+++ hello.php
@@ -3,3 +3,4 @@ echo "<p>My php version is: " . phpversion() . "</p>";
 echo "<p>This is test from docker and </p>";
 ?>
 <a href="index.php">Click here to go to index.php</a>
+<p>Welcome to my website</p>
