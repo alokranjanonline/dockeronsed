87d251e1d378be0be8822bbfc1ac847237e889a2
aloksport <alokranjan999@gmail.com>
2026-09-14 20:09:38 +0530

    Build and push Docker image

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//phpinfo();
+phpinfo();
 echo "<p>My php version is: " . phpversion() . "</p>";
 echo "<p>This is test from docker and </p>";
 echo "<p>Welcome to my website</p>";
@@ -7,5 +7,6 @@ echo "<p>This is a simple PHP script running in a Docker container.</p>";
 echo "<p>You can customize this page to display your own content.</p>";
 echo "<p>Feel free to modify the code and add your own features.</p>";
 echo "<p>Thank you for visiting!</p>";
+echo "<p>My hostname is: " . gethostname() . "</p>";
 ?>
 <a href="hello.php">Click here to go to hello.php</a>
