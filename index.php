a1bd29a164a5f728cfa092bd74d1bee6d816646b
aloksport <alokranjan999@gmail.com>
2026-09-14 22:47:49 +0530

    Test at 10:47PM

--- index.php
+++ index.php
@@ -13,3 +13,14 @@ echo "<p>My server name is: " . $_SERVER['SERVER_NAME'] . "</p>";
 ?>
 <a href="hello.php">Click here to go to hello.php</a>
 <p>Welcome to my website</p><p>I updated the permission on docker hub</p>
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+if (!$mysqli) { 
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Connected successfully to the database.";
+}
